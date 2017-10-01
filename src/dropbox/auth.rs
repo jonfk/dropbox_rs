a9ac7409925fff665083db7db2401a095e972f64
Jonathan Fok kan <jfokkan@gmail.com>
2017-10-01 17:46:16 -0400

    Added error handling to main

--- src/dropbox/auth.rs
+++ src/dropbox/auth.rs
@@ -26,56 +26,50 @@ pub enum AuthorizationResponse {
         token_type: String,
         uid: String,
         account_id: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
         team_id: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
         state: Option<String>,
     },
     CodeResponse { code: String, state: Option<String> },
 }
 
 pub fn parse_authorization_response(redirect_uri: &str) -> Option<AuthorizationResponse> {
-    let mut url = Url::parse(redirect_uri).unwrap();
-    if url.query().is_none() {
-        let fragment = url.fragment().map(|x| String::from(x));
-        url.set_query(fragment.as_ref().map(|x| x.as_str()));
+    match Url::parse(redirect_uri) {
+        Err(_) => None,
+        Ok(mut url) => {
+            if url.query().is_none() {
+                let fragment = url.fragment().map(|x| String::from(x));
+                url.set_query(fragment.as_ref().map(|x| x.as_str()));
+            }
+
+            let mut query_pairs = url.query_pairs().fold(HashMap::new(), |mut pairs, query_pair| {
+                let (a, b) = query_pair;
+                pairs.insert(a.into_owned(), b.into_owned());
+                pairs
+            });
+
+            // TODO deserialize with serde_urlencoded
+            if query_pairs.contains_key("code") {
+                Some(CodeResponse {
+                    code: query_pairs.remove("code").unwrap(),
+                    state: query_pairs.remove("state"),
+                })
+            } else if query_pairs.contains_key("access_token") {
+                Some(TokenResponse {
+                    access_token: query_pairs.remove("access_token").unwrap(),
+                    token_type: query_pairs.remove("token_type").unwrap(),
+                    uid: query_pairs.remove("uid").unwrap_or("".to_owned()),
+                    account_id: query_pairs.remove("account_id").unwrap_or("".to_owned()),
+                    team_id: query_pairs.remove("team_id"),
+                    state: query_pairs.remove("state"),
+                })
+            } else {
+                None
+            }
+        }
     }
 
-    let mut query_pairs = url.query_pairs().fold(HashMap::new(), |mut pairs, query_pair| {
-        let (a, b) = query_pair;
-        pairs.insert(a.into_owned(), b.into_owned());
-        pairs
-    });
-
-    // TODO deserialize with serde_urlencoded
-    if query_pairs.contains_key("code") {
-        Some(CodeResponse {
-            code: query_pairs.remove("code").unwrap(),
-            state: query_pairs.remove("state"),
-        })
-    } else if query_pairs.contains_key("access_token") {
-        Some(TokenResponse {
-            access_token: query_pairs.remove("access_token").unwrap(),
-            token_type: query_pairs.remove("token_type").unwrap(),
-            uid: query_pairs.remove("uid").unwrap_or("".to_owned()),
-            account_id: query_pairs.remove("account_id").unwrap_or("".to_owned()),
-            team_id: query_pairs.remove("team_id"),
-            state: query_pairs.remove("state"),
-        })
-    } else {
-        None
-    }
-}
-
-pub fn run_authorization_flow(client_id: &str,
-                              redirect_uri: &str,
-                              response_type: &str)
-                              -> Option<AuthorizationResponse> {
-    println!("Please visit the following url and authorize this app {}",
-             build_authorization_uri(client_id, redirect_uri, response_type));
-    print!("Paste  redirect url here : ");
-    io::stdout().flush().unwrap();
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    parse_authorization_response(&buffer)
 }
 
 #[derive(Serialize, Deserialize)]
--- src/main.rs
+++ src/main.rs
@@ -12,27 +12,66 @@ extern crate error_chain;
 
 mod dropbox;
 
-use dropbox::auth::{run_authorization_flow, AuthOperations};
+use dropbox::auth::AuthOperations;
 use dropbox::auth::AuthorizationResponse::{CodeResponse, TokenResponse};
-use dropbox::auth::AuthTokenRequest;
+use dropbox::auth::{AuthTokenRequest, AuthorizationResponse};
 use dropbox::Dropbox;
 use dropbox::paper::{ListPaperDocsRequest, ListPaperDocsSortBy};
+use dropbox::errors::*;
 
 use reqwest::{Url, Client};
+
+use std::io::{self, Write};
 
 static CLIENT_ID: &'static str = "rfias7v8pyuu83k";
 static CLIENT_SECRET: &'static str = "nwq1ootdp0kvv9r";
 static REDIRECT_URI: &'static str = "http://localhost";
 
 fn main() {
+    let test = run();
+    println!("{:?}", test);
+    if let Err(ref e) = test {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
 
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run_authorization_flow(client_id: &str,
+                          redirect_uri: &str,
+                          response_type: &str)
+                          -> Option<AuthorizationResponse> {
+    println!("Please visit the following url and authorize this app {}",
+             dropbox::auth::build_authorization_uri(client_id, redirect_uri, response_type));
+    print!("Paste  redirect url here : ");
+    io::stdout().flush().unwrap();
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    dropbox::auth::parse_authorization_response(&buffer)
+}
+
+fn run() -> Result<()> {
     let token = run_authorization_flow(CLIENT_ID, REDIRECT_URI, "token").unwrap();
-    println!("{:?}", token);
     let auth_ops = AuthOperations::new(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
     match token {
         CodeResponse { ref code, .. } => {
-            let token = auth_ops.fetch_token(code).unwrap();
+            let token = auth_ops.fetch_token(code)?;
             println!("{:?}", token);
+            Ok(())
         }
         TokenResponse { ref access_token, .. } => {
             let api = Dropbox::new(access_token);
@@ -44,6 +83,7 @@ fn main() {
             });
 
             println!("list : {:?}", list);
+            Ok(())
         }
     }
 }
